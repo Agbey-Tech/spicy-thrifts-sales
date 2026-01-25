e52d33168454a8f7110e7fd836761ffdbca7ce57
ZiglaCity <ziglacity@gmail.com>
2026-01-25 03:44:42 +0000

    clean up requireAuth

--- lib/auth/requireAuth.ts
+++ lib/auth/requireAuth.ts
@@ -1,13 +1,6 @@
-import { NextRequest } from "next/server";
 import { createSupabaseServerClient } from "../supabase/server";
 
-export async function requireAuth(req: NextRequest) {
-  // const accessToken = req.headers.get("authorization")?.replace("Bearer ", "");
-  // if (!accessToken) throw new Error("Unauthorized");
-  // const supabase = createServerClient(accessToken);
-  // const { data, error } = await supabase.auth.getUser();
-  // if (error || !data?.user) throw new Error("Unauthorized");
-  // return data.user;
+export async function requireAuth() {
   const supabase = await createSupabaseServerClient();
 
   const {
