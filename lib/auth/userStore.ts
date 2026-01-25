2c9c4a187b1d3305498c2fe5c64a09f6e3e504dd
ZiglaCity <ziglacity@gmail.com>
2026-01-25 01:22:45 +0000

    Add userStore

---
+++ lib/auth/userStore.ts
@@ -0,0 +1,20 @@
+import { create } from "zustand";
+
+export type UserProfile = {
+  id: string;
+  name: string;
+  role: "ADMIN" | "SALES";
+  email: string;
+};
+
+type UserStore = {
+  user: UserProfile | null;
+  setUser: (user: UserProfile) => void;
+  clearUser: () => void;
+};
+
+export const useUserStore = create<UserStore>((set) => ({
+  user: null,
+  setUser: (user) => set({ user }),
+  clearUser: () => set({ user: null }),
+}));
